ce11073f40bd69b2246523d09e2492a43dec77a9
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-03-23 19:02:07 -0300

    App Contatos: Adicionada imagem para lista vazia

---
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/ContactsListScreen.kt
@@ -0,0 +1,77 @@
+package br.edu.utfpr.appcontatos.ui.contact
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
+
+@Composable
+fun ContactsListScreen(modifier: Modifier = Modifier) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        title = {
+            Text("Contatos")
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppBarPreview() {
+    AppContatosTheme {
+        AppBar()
+
+    }
+}
+
+@Composable
+fun LoadingState(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = modifier.size(60.dp)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Carregando contatos...",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun LoadingStatePreview() {
+    AppContatosTheme {
+        LoadingState()
+    }
+}
