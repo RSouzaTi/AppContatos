4b1e8dfa8c3d36dfa5f630aac4b94a9f8f1f9eea
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-04-04 15:24:58 -0300

    App Contatos: Movido estado do componente ContactListItem

--- app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/ContactsListScreen.kt
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/ContactsListScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButtonDefaults.colors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -47,15 +46,15 @@ import br.edu.utfpr.appcontatos.R
 import br.edu.utfpr.appcontatos.data.Contact
 import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
+
 @Composable
 fun ContactsListScreen(
     modifier: Modifier = Modifier,
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
 ) {
     val isInitialCompositionState: MutableState<Boolean> = rememberSaveable {
         mutableStateOf(true)
@@ -64,14 +63,14 @@ fun ContactsListScreen(
         mutableStateOf(false)
 
     }
-    var isErrorState: MutableState<Boolean> = rememberSaveable {
+    val isErrorState: MutableState<Boolean> = rememberSaveable {
         mutableStateOf(false)
     }
-    var contactsState: MutableState<List<Contact>> = rememberSaveable {
-        mutableStateOf(emptyList())
+    val contactsState: MutableState<List<Contact>> = rememberSaveable {
+        mutableStateOf(listOf())
     }
 
-    val loadingContacts: () -> Unit = {
+    val loadContacts: () -> Unit = {
         isLoadingState.value = true
         isErrorState.value = false
 
@@ -80,7 +79,7 @@ fun ContactsListScreen(
             isErrorState.value = Random.nextBoolean()
             if (!isErrorState.value) {
                 val isEmpty = Random.nextBoolean()
-                if (!isEmpty) {
+                if (isEmpty) {
                     contactsState.value = listOf()
                 } else {
                     contactsState.value = generateContacts()
@@ -90,8 +89,18 @@ fun ContactsListScreen(
 
         }
     }
+    val toggleIsFavorite: (Contact) -> Unit = { updateContact ->
+        contactsState.value = contactsState.value.map { currentContact ->
+            if (currentContact.id == updateContact.id) {
+                currentContact.copy(isFavorite = !currentContact.isFavorite)
+            } else {
+                currentContact
+            }
+        }
+    }
+
     if (isInitialCompositionState.value) {
-        loadingContacts()
+        loadContacts()
         isInitialCompositionState.value = false
     }
     val contentModifier = modifier.fillMaxSize()
@@ -100,301 +109,303 @@ fun ContactsListScreen(
     } else if (isErrorState.value) {
         ErrorState(
             modifier = contentModifier,
-            omTryAgainPresses = loadingContacts
+            onTryAgainPressed = loadContacts
         )
     } else {
         Scaffold(
             modifier = modifier.fillMaxSize(),
             topBar = {
                 AppBar(
-                onRefreshPressed = loadingContacts
-            )
-
-           },
+                    onRefreshPressed = loadContacts
+                )
+            },
             floatingActionButton = {
                 ExtendedFloatingActionButton(onClick = {
                     contactsState.value = contactsState.value.plus(
                         Contact(firstName = "Teste", lastName = "Teste")
-                        )
-                     }
-                )
-             {
-                  Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "Novo contato"
                     )
-                 Spacer(Modifier.size(8.dp))
-                 Text("Novo contato")
+                }
+                )
+                {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "Adicionar"
+                    )
+                    Spacer(Modifier.size(8.dp))
+                    Text("Novo contato")
                 }
             }
 
 
         ) { paddingValues ->
             val defaultModifier: Modifier = Modifier.padding(paddingValues)
-           if (contactsState.value.isEmpty()) {
+            if (contactsState.value.isEmpty()) {
                 EmptyList(modifier = defaultModifier)
             } else {
                 List(
                     modifier = defaultModifier,
-                    contacts = contactsState.value
+                    contacts = contactsState.value,
+                    onFavoritePressed = toggleIsFavorite
                 )
             }
         }
     }
 }
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    fun AppBar(
-        modifier: Modifier = Modifier,
-        onRefreshPressed: () -> Unit
-    ) {
-        TopAppBar(
-            modifier = modifier.fillMaxWidth(),
-            title = {
-                Text("Contatos")
-            },
-           colors = TopAppBarDefaults.topAppBarColors(
-               titleContentColor = MaterialTheme.colorScheme.primary,
-               actionIconContentColor = MaterialTheme.colorScheme.primary
-           ),
-            actions = {
-                IconButton(onClick = onRefreshPressed) {
-                    Icon(
-                        imageVector = Icons.Filled.Refresh,
-                        contentDescription = "Atualizar"
-                    )
-                }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    modifier: Modifier = Modifier,
+    onRefreshPressed: () -> Unit
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text("Contatos")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            titleContentColor = MaterialTheme.colorScheme.primary,
+            actionIconContentColor = MaterialTheme.colorScheme.primary
+        ),
+        actions = {
+            IconButton(onClick = onRefreshPressed) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = "Atualizar"
+                )
             }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppBarPreview() {
+    AppContatosTheme {
+        AppBar(
+            onRefreshPressed = {}
+        )
+
+    }
+}
+
+@Composable
+fun LoadingState(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            modifier = Modifier.size(60.dp)
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = "Carregando contatos...",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
         )
     }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun AppBarPreview() {
-        AppContatosTheme {
-            AppBar(
-                onRefreshPressed = {}
-            )
+}
 
-        }
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun LoadingStatePreview() {
+    AppContatosTheme {
+        LoadingState()
     }
+}
 
-    @Composable
-    fun LoadingState(modifier: Modifier = Modifier) {
-        Column(
-            modifier = modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            CircularProgressIndicator(
-                color = MaterialTheme.colorScheme.primary,
-                trackColor = MaterialTheme.colorScheme.surfaceVariant,
-                modifier = modifier.size(60.dp)
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Text(
-                text = "Carregando contatos...",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary
-            )
-        }
-
-    }
-
-    @Preview(showBackground = true, heightDp = 400)
-    @Composable
-    fun LoadingStatePreview() {
-        AppContatosTheme {
-            LoadingState()
-        }
-    }
-
-    @Composable
-    fun ErrorState(
-        modifier: Modifier = Modifier,
-        omTryAgainPresses: () -> Unit
+@Composable
+fun ErrorState(
+    modifier: Modifier = Modifier,
+    onTryAgainPressed: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            modifier = modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Icon(
+            imageVector = Icons.Filled.CloudOff,
+            contentDescription = "Erro ao carregar",
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(80.dp)
+        )
+        val textPadding = PaddingValues(
+            top = 8.dp,
+            start = 8.dp,
+            end = 8.dp
+        )
+        Text(
+            modifier = Modifier.padding(textPadding),
+            text = "Ocorreu um erro ao carregar",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        Text(
+            modifier = Modifier.padding(textPadding),
+            text = "Aguarde um momento e tente novamente",
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        ElevatedButton(
+            modifier = Modifier.padding(top = 16.dp),
+            onClick = onTryAgainPressed
         ) {
-            Icon(
-                imageVector = Icons.Filled.CloudOff,
-                contentDescription = "Erro ao carregar",
-                tint = MaterialTheme.colorScheme.primary,
-                modifier = modifier.size(80.dp)
+            Text("Tentar novamente")
+        }
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun ErrorStatePreview() {
+    AppContatosTheme {
+        ErrorState(
+            onTryAgainPressed = {}
+        )
+    }
+}
+
+@Composable
+fun EmptyList(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Image(
+            painter = painterResource(R.drawable.no_data),
+            contentDescription = "Nada por aqui",
+        )
+        Text(
+            text = "Nenhum por aqui",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = modifier.padding(top = 16.dp)
+
+        )
+        Text(
+            text = "Você ainda não adicionou nenhum contato." +
+                    "\nAdicione um contato clicando no botão \"Novo contato\"",
+            modifier = modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+
             )
-            val textPadding = PaddingValues(
-                top = 8.dp,
-                start = 8.dp,
-                end = 8.dp,
+    }
+}
+
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+fun EmptyListPreview() {
+    AppContatosTheme {
+        EmptyList()
+    }
+
+}
+
+@Composable
+fun List(
+    modifier: Modifier = Modifier,
+    contacts: List<Contact> = emptyList(),
+    onFavoritePressed: (Contact) -> Unit
+
+) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        items(contacts) { contact ->
+            ContactListItem(
+                contact = contact,
+                onFavoritePressed = onFavoritePressed
             )
-            Text(
-                modifier = modifier.padding(textPadding),
-                text = "Ocorreu um erro ao carregar",
-                style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary
-            )
-            Text(
-                modifier = modifier.padding(textPadding),
-                text = "Aguarde um momento e tente novamente",
-                style = MaterialTheme.typography.titleSmall,
-                color = MaterialTheme.colorScheme.primary
-            )
-            ElevatedButton(
-                modifier = modifier.padding(top = 16.dp),
-                onClick = omTryAgainPresses
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListPreview() {
+    AppContatosTheme {
+        List(
+            contacts = generateContacts(),
+            onFavoritePressed = {}
+
+        )
+    }
+}
+
+@Composable
+fun ContactListItem(
+    modifier: Modifier = Modifier,
+    contact: Contact,
+    onFavoritePressed: (Contact) -> Unit
+) {
+    ListItem(
+        modifier = modifier,
+        headlineContent = {
+            Text(contact.fullName)
+        },
+        trailingContent = {
+            IconButton(
+                onClick = {
+                    onFavoritePressed(contact)
+                }
             ) {
-                Text("Tentar novamente")
-            }
-        }
-    }
-
-    @Preview(showBackground = true, heightDp = 400)
-    @Composable
-    fun ErrorStatePreview() {
-        AppContatosTheme {
-            ErrorState(
-                omTryAgainPresses = {}
-            )
-        }
-    }
-
-    @Composable
-    fun EmptyList(modifier: Modifier = Modifier) {
-        Column(
-            modifier = modifier
-                .padding(16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.no_data),
-                contentDescription = "Nenhumpor aqui",
-            )
-            Text(
-                text = "Nenhum por aqui",
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.primary,
-                textAlign = TextAlign.Center,
-                modifier = modifier.padding(top = 16.dp)
-
-            )
-            Text(
-                text = "Você ainda não adicionou nenhum contato." +
-                        "\nAdicione um contato clicando no botão \"Novo contato\"",
-                modifier = modifier.padding(top = 16.dp),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.primary,
-                textAlign = TextAlign.Center,
-
-                )
-        }
-    }
-
-    @Preview(showBackground = true, heightDp = 400)
-    @Composable
-    fun EmptyListPreview() {
-        AppContatosTheme {
-            EmptyList()
-        }
-
-    }
-
-    @Composable
-    fun List(
-        modifier: Modifier = Modifier,
-        contacts: List<Contact> = emptyList()
-
-    ) {
-        LazyColumn(
-            modifier = modifier
-                .fillMaxSize()
-        ) {
-            items(contacts) { contact ->
-                ContactListItem(contact = contact)
-            }
-        }
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun ListPreview() {
-        AppContatosTheme {
-            List(
-                contacts = generateContacts()
-
-            )
-        }
-    }
-
-    @Composable
-    fun ContactListItem(
-        modifier: Modifier = Modifier,
-        contact: Contact
-    ) {
-        val isFavoriteState: MutableState<Boolean> = rememberSaveable {
-            mutableStateOf(contact.isFavorite)
-        }
-
-        ListItem(
-            modifier = modifier,
-            headlineContent = {
-                Text(contact.fullName)
-            },
-            trailingContent = {
-                IconButton(
-                    onClick = {
-                        isFavoriteState.value = !isFavoriteState.value
+                Icon(
+                    imageVector = if (contact.isFavorite) {
+                        Icons.Filled.Favorite
+                    } else {
+                        Icons.Filled.FavoriteBorder
+                    },
+                    contentDescription = "Favoritar",
+                    tint = if (contact.isFavorite) {
+                        Color.Red
+                    } else {
+                        LocalContentColor.current
                     }
-                ) {
-                    Icon(
-                        imageVector = if (isFavoriteState.value) {
-                            Icons.Filled.Favorite
-                        } else {
-                            Icons.Filled.FavoriteBorder
-                        },
-                        contentDescription = "Favoritar",
-                        tint = if (isFavoriteState.value) {
-                            Color.Red
-                        } else {
-                            LocalContentColor.current
-                        }
-                    )
-                }
-            }
-        )
-    }
-
-    fun generateContacts(): List<Contact> {
-        val firstNames = listOf(
-            "João", "José", "Everton", "Marcos", "André", "Anderson", "Antônio",
-            "Laura", "Ana", "Maria", "Joaquina", "Suelen"
-        )
-        val lastNames = listOf(
-            "Do Carmo", "Oliveira", "Dos Santos", "Da Silva", "Brasil", "Pichetti",
-            "Cordeiro", "Silveira", "Andrades", "Cardoso"
-        )
-        val contacts: MutableList<Contact> = mutableListOf()
-        for (i in 0..29) {
-            var generatedNewContact = false
-            while (!generatedNewContact) {
-                val firstNameIndex = Random.nextInt(firstNames.size)
-                val lastNameIndex = Random.nextInt(lastNames.size)
-                val newContact = Contact(
-                    id = i + 1,
-                    firstName = firstNames[firstNameIndex],
-                    lastName = lastNames[lastNameIndex]
                 )
-                if (!contacts.any { it.fullName == newContact.fullName }) {
-                    contacts.add(newContact)
-                    generatedNewContact = true
-                }
             }
         }
-        return contacts
+    )
+}
+
+private fun generateContacts(): List<Contact> {
+    val firstNames = listOf(
+        "João", "José", "Everton", "Marcos", "André", "Anderson", "Antônio",
+        "Laura", "Ana", "Maria", "Joaquina", "Suelen"
+    )
+    val lastNames = listOf(
+        "Do Carmo", "Oliveira", "Dos Santos", "Da Silva", "Brasil", "Pichetti",
+        "Cordeiro", "Silveira", "Andrades", "Cardoso"
+    )
+    val contacts: MutableList<Contact> = mutableListOf()
+    for (i in 0..29) {
+        var generatedNewContact = false
+        while (!generatedNewContact) {
+            val firstNameIndex = Random.nextInt(firstNames.size)
+            val lastNameIndex = Random.nextInt(lastNames.size)
+            val newContact = Contact(
+                id = i + 1,
+                firstName = firstNames[firstNameIndex],
+                lastName = lastNames[lastNameIndex]
+            )
+            if (!contacts.any { it.fullName == newContact.fullName }) {
+                contacts.add(newContact)
+                generatedNewContact = true
+            }
+        }
     }
+    return contacts
+}
