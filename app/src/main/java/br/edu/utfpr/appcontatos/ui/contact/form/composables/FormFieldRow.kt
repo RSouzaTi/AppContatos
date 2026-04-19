fbbc5ea81dd824284d3c6de04b15b88e9921c226
Ricardo José de Souza <ti.ricardo.souza@gmail.com>
2026-04-18 22:12:11 -0300

    App Contatos: Criado componente FormFieldRow

---
+++ app/src/main/java/br/edu/utfpr/appcontatos/ui/contact/form/composables/FormFieldRow.kt
@@ -0,0 +1,61 @@
+package br.edu.utfpr.appcontatos.ui.contact.form.composables
+
+import android.R.attr.contentDescription
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Brightness1
+import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.utfpr.appcontatos.ui.theme.AppContatosTheme
+
+@Composable
+fun FormFiledRow(
+    modifier: Modifier = Modifier,
+    label: String,
+    imageVector: ImageVector? = null,
+    content: @Composable () -> Unit,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Icon(
+            imageVector = imageVector ?: Icons.Default.Brightness1,
+            contentDescription = label,
+            tint = if (imageVector == null) {
+                MaterialTheme.colorScheme.background
+                } else {
+                MaterialTheme.colorScheme.outline
+            }
+        )
+            content()
+        }
+    }
+@Preview(showBackground = true, heightDp = 400)
+@Composable
+private fun FormFiledRowPreview(){
+    AppContatosTheme {
+        FormFiledRow(
+            label = "Nome",
+            imageVector = Icons.Outlined.Person,
+            content = {
+                FormTextField(
+                    label = "Nome",
+                    value = "",
+                    onValueChange = {}
+                )
+
+            }
+        )
+    }
+}
